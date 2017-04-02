1c9ed6675e91821c31ded900c5cb73396155b238
Shuiloon Yip <shuoodle@gmail.com>
2017-04-02 21:21:43 +0100

    Add assign correct color

--- src/script.js
+++ src/script.js
@@ -1,7 +1,28 @@
-var square = document.querySelectorAll('.square');
+var correctColor;
+var colorSet;
+var gameMode = 6;
 
-for (var i = 0; i < square.length; i++) {
-  square[i].style.background = getRandomColor();
+var square = document.querySelectorAll('.square');
+var headerRgb = document.querySelector('.header__rgb');
+function init() {
+  colorSet = getColorSet(gameMode);
+
+  var randomNum = getRandom(0, colorSet.length - 1);
+  correctColor = colorSet[randomNum];
+  headerRgb.textContent = correctColor;
+
+  for (var i = 0; i < square.length; i++) {
+    square[i].style.background = colorSet[i];
+    square[i].textContent = colorSet[i]; //testing
+  }
+}
+
+function getColorSet(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push(getRandomColor());
+  }
+  return arr;
 }
 
 function getRandomColor() {
@@ -15,3 +36,5 @@ function getRandomColor() {
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+init();
